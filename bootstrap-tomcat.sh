20775f0ccaa07a972ddd5e138e00e8eece475e8a
Nada Hassan <110364476+Nadasawah90@users.noreply.github.com>
2025-07-31 15:37:08 +0300

    Add files via upload

--- bootstrap-tomcat.sh
+++ bootstrap-tomcat.sh
@@ -1,14 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-# Install Tomcat 
+# Install dependencies
 sudo yum update -y  
-sudo dnf -y install java-11-openjdk java-11-openjdk-devel
-sudo dnf install git maven wget -y
-sudo cd /tmp/ && sudo wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz && tar  xzvf apache-tomcat-9.0.75.tar.gz
-sudo useradd --home-dir /usr/local/tomcat --shell /sbin/nologin tomcat
-sudo cp -r /tmp/apache-tomcat-9.0.75/* /usr/local/tomcat/
-sudo chown -R tomcat.tomcat /usr/local/tomcat
+sudo dnf install -y java-11-openjdk java-11-openjdk-devel git maven wget
 
+# Download Tomcat
+cd /tmp
+wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz
+tar xzvf apache-tomcat-9.0.75.tar.gz
+
+# Setup Tomcat user and directory
+sudo useradd --home-dir /usr/local/tomcat --shell /sbin/nologin tomcat
+sudo mkdir -p /usr/local/tomcat
+sudo cp -r /tmp/apache-tomcat-9.0.75/* /usr/local/tomcat/
+sudo chown -R tomcat:tomcat /usr/local/tomcat
+
+# Create systemd service
 cat << 'EOF' | sudo tee /etc/systemd/system/tomcat.service > /dev/null
 [Unit]
 Description=Tomcat
@@ -28,17 +35,27 @@ SyslogIdentifier=tomcat-%i
 [Install]
 WantedBy=multi-user.target
 EOF
+
+# Start Tomcat
 sudo systemctl daemon-reload
 sudo systemctl start tomcat
 sudo systemctl enable tomcat
-sudo git clone -b main https://github.com/hkhcoder/vprofile-project.git
-sudo cd vprofile-project
-vim src/main/resources/application.properties
+
+# Clone app and build
+cd ~
+git clone -b main https://github.com/hkhcoder/vprofile-project.git
+cd vprofile-project
+vim src/main/resources/application.properties   # manual step or use `sed`
 sudo systemctl stop tomcat
 sudo mvn install
-sudo  rm -rf /usr/local/tomcat/webapps/ROOT*
+
+# Deploy WAR
+sudo rm -rf /usr/local/tomcat/webapps/ROOT*
 sudo cp target/vprofile-v2.war /usr/local/tomcat/webapps/ROOT.war
-sudo chown tomcat.tomcat /usr/local/tomcat/webapps -R
+sudo chown -R tomcat:tomcat /usr/local/tomcat/webapps
+
+# Remove Java 17 if exists
 sudo yum remove -y java-17-openjdk*
-sudo  systemctl start tomcat
+
+# Start Tomcat
 sudo systemctl restart tomcat
